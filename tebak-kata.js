8a8efa68aa61f43f51210e297dbc22df5e6cd4c1
julindramarkus <julindramarkus11@gmail.com>
2017-08-29 10:16:59 +0700

    tebak-kata

---
+++ tebak-kata.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const readline = require('readline');
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+console.log('Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!!');
+
+
+var dataJson = JSON.parse(fs.readFileSync('data.json'));
+var next = 0
+
+rl.setPrompt("soal: " + dataJson[next].definition + "\nJawaban: ");
+rl.prompt();
+
+rl.on('line', (answer) => {
+  if(dataJson.length-1 > next){ //karna pertanyaannya ada 4 saja(index=3), dan karna ada next++, dia harus distop dengan -1
+    if(answer.trim().toLowerCase() == dataJson[next].term.toLowerCase()){
+      console.log('damn you are right!! There are another questions tho ');
+      next++;
+      rl.setPrompt("soal: " + dataJson[next].definition + "\nJawaban: ");
+    } else {
+      console.log('salah');
+    }
+    rl.prompt();
+  }else {
+    console.log('anda menang');
+    rl.close();
+  }
+}).on('close', () => {
+  console.log('Good bye madafaka!');
+  process.exit(0);
+});
+
+// fs.readFile('/etc/passwd', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
